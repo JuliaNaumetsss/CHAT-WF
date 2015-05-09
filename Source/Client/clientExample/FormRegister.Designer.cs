5eba86e438078237cacef0657786b26786d83f61
JuliaNaumets <julianaumets@github.com>
2015-05-09 19:13:53 +0300

    first commit

---
+++ Source/Client/clientExample/FormRegister.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace clientExample
+{
+    partial class FormRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupRegister = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupRegister
+            // 
+            this.groupRegister.Location = new System.Drawing.Point(12, 30);
+            this.groupRegister.Name = "groupRegister";
+            this.groupRegister.Size = new System.Drawing.Size(200, 100);
+            this.groupRegister.TabIndex = 0;
+            this.groupRegister.TabStop = false;
+            this.groupRegister.Text = "Register";
+            // 
+            // FormRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 174);
+            this.Controls.Add(this.groupRegister);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupRegister;
+    }
+}
