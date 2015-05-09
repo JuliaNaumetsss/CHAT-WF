7762f837da5aa4fd76a9a8825c6b0577b990f4d3
JuliaNaumets <julianaumets@github.com>
2015-05-09 22:31:43 +0300

    Class Client

--- Source/Client/clientExample/FormClient.Designer.cs
+++ Source/Client/clientExample/FormClient.Designer.cs
@@ -28,20 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupConnect = new System.Windows.Forms.GroupBox();
+            this.textBoxIp = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.buttonChangeNick = new System.Windows.Forms.Button();
+            this.buttonChangePass = new System.Windows.Forms.Button();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.labelIp = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelOnline = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.groupConnect.SuspendLayout();
+            this.groupBox.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupConnect
+            // 
+            this.groupConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupConnect.Controls.Add(this.label2);
+            this.groupConnect.Controls.Add(this.label1);
+            this.groupConnect.Controls.Add(this.labelIp);
+            this.groupConnect.Controls.Add(this.buttonCreate);
+            this.groupConnect.Controls.Add(this.buttonConnect);
+            this.groupConnect.Controls.Add(this.textBoxPassword);
+            this.groupConnect.Controls.Add(this.textBoxEmail);
+            this.groupConnect.Controls.Add(this.textBoxIp);
+            this.groupConnect.Location = new System.Drawing.Point(12, 12);
+            this.groupConnect.Name = "groupConnect";
+            this.groupConnect.Size = new System.Drawing.Size(314, 127);
+            this.groupConnect.TabIndex = 12;
+            this.groupConnect.TabStop = false;
+            this.groupConnect.Text = "Connection Detail";
+            // 
+            // textBoxIp
+            // 
+            this.textBoxIp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxIp.Location = new System.Drawing.Point(98, 19);
+            this.textBoxIp.Name = "textBoxIp";
+            this.textBoxIp.Size = new System.Drawing.Size(203, 20);
+            this.textBoxIp.TabIndex = 0;
+            this.textBoxIp.Text = "127.0.0.1";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEmail.Location = new System.Drawing.Point(98, 42);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(203, 20);
+            this.textBoxEmail.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPassword.Location = new System.Drawing.Point(98, 68);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(203, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConnect.Location = new System.Drawing.Point(6, 100);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(203, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Login";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCreate.Location = new System.Drawing.Point(215, 100);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(90, 23);
+            this.buttonCreate.TabIndex = 10;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox.Controls.Add(this.buttonSend);
+            this.groupBox.Controls.Add(this.labelOnline);
+            this.groupBox.Controls.Add(this.textBoxMessage);
+            this.groupBox.Controls.Add(this.messageBox);
+            this.groupBox.Controls.Add(this.buttonChangePass);
+            this.groupBox.Controls.Add(this.buttonChangeNick);
+            this.groupBox.Controls.Add(this.buttonLogout);
+            this.groupBox.Enabled = false;
+            this.groupBox.Location = new System.Drawing.Point(12, 141);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(311, 341);
+            this.groupBox.TabIndex = 11;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Chat";
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLogout.Location = new System.Drawing.Point(203, 19);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(102, 22);
+            this.buttonLogout.TabIndex = 12;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            // 
+            // buttonChangeNick
+            // 
+            this.buttonChangeNick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChangeNick.Location = new System.Drawing.Point(203, 41);
+            this.buttonChangeNick.Name = "buttonChangeNick";
+            this.buttonChangeNick.Size = new System.Drawing.Size(102, 22);
+            this.buttonChangeNick.TabIndex = 13;
+            this.buttonChangeNick.Text = "Edit Nickname";
+            this.buttonChangeNick.UseVisualStyleBackColor = true;
+            // 
+            // buttonChangePass
+            // 
+            this.buttonChangePass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChangePass.Location = new System.Drawing.Point(203, 64);
+            this.buttonChangePass.Name = "buttonChangePass";
+            this.buttonChangePass.Size = new System.Drawing.Size(102, 22);
+            this.buttonChangePass.TabIndex = 14;
+            this.buttonChangePass.Text = "Edit Password";
+            this.buttonChangePass.UseVisualStyleBackColor = true;
+            // 
+            // messageBox
+            // 
+            this.messageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageBox.BackColor = System.Drawing.Color.White;
+            this.messageBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.messageBox.Location = new System.Drawing.Point(6, 19);
+            this.messageBox.Multiline = true;
+            this.messageBox.Name = "messageBox";
+            this.messageBox.ReadOnly = true;
+            this.messageBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messageBox.Size = new System.Drawing.Size(191, 280);
+            this.messageBox.TabIndex = 5;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(6, 305);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(191, 20);
+            this.textBoxMessage.TabIndex = 6;
+            // 
+            // labelIp
+            // 
+            this.labelIp.AutoSize = true;
+            this.labelIp.Location = new System.Drawing.Point(11, 22);
+            this.labelIp.Name = "labelIp";
+            this.labelIp.Size = new System.Drawing.Size(59, 13);
+            this.labelIp.TabIndex = 3;
+            this.labelIp.Text = "Server OP:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Your Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Your Password:";
+            // 
+            // labelOnline
+            // 
+            this.labelOnline.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelOnline.AutoSize = true;
+            this.labelOnline.Location = new System.Drawing.Point(200, 89);
+            this.labelOnline.Name = "labelOnline";
+            this.labelOnline.Size = new System.Drawing.Size(70, 13);
+            this.labelOnline.TabIndex = 11;
+            this.labelOnline.Text = "Online Users:";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(203, 305);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(98, 23);
+            this.buttonSend.TabIndex = 7;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(334, 494);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.groupConnect);
             this.Name = "FormMain";
             this.Text = "Client";
+            this.groupConnect.ResumeLayout(false);
+            this.groupConnect.PerformLayout();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupConnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelIp;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxIp;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label labelOnline;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Button buttonChangePass;
+        private System.Windows.Forms.Button buttonChangeNick;
+        private System.Windows.Forms.Button buttonLogout;
     }
 }
 
--- Source/Client/clientExample/FormClient.cs
+++ Source/Client/clientExample/FormClient.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿
 namespace clientExample
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
     public partial class FormMain : Form
     {
+        public static Client Client = new Client();
+
+        [DllImport("user32.dll", EntryPoint = "SendMessageA")]
+        private static extern uint ScrollText(IntPtr hwnd, uint wMsg, uint wParam, uint lParam);
+        private const int WM_VSCROLL = 0x115;
+        private const int SB_BOTTOM = 7;
+
+        public static string IPADDRESS = "127.0.0.1";
+        public const int PORT = 1987;
+        public static string CurrentNickname = "";
+        public static int CurrentUserId = 0;
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
