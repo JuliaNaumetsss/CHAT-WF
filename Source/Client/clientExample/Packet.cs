e4136e9979d6606ea250982df739e9b104bc7ee4
JuliaNaumets <julianaumets@github.com>
2015-05-16 20:44:46 +0300

    Serialize/Desirialize

--- Source/Client/clientExample/Packet.cs
+++ Source/Client/clientExample/Packet.cs
@@ -33,53 +33,5 @@ namespace clientExample
         public string Carriage { get; set; }
     }
 
-    public static class Packets
-    {
-        public static Packet UpdateRole(string userid, Sets.UserRole newRole)
-        {
-            var package = new List<string>
-                              {
-                                 userid,
-                                 newRole.ToString()
-                              };
-            return new Packet("UPDATE ROLE", package.Package());
-        }
-        public static Packet UpdateRole(string userid, string newRole)
-        {
-            var package = new List<string>
-                              {
-                                 userid,
-                                 newRole
-                              };
-            return new Packet("UPDATE ROLE", package.Package());
-        }
-        public static Packet UpdatePassword(string newpass)
-        {
-            return new Packet("UPDATE PASSWORD", newpass);
-        }
-        public static Packet CreateRequest(string email, string password, string nickname)
-        {
-            var package = new List<string>
-                              {
-                                  email,
-                                  password,
-                                  nickname
-                              };
-            return new Packet("CREATE REQUEST", package.Package());
-        }
-        public static Packet UpdateNickname(string newNickname)
-        {
-            return new Packet("UPDATE NICKNAME", newNickname);
-        }
-        public static Packet Kill(string userId)
-        {
-            return new Packet("KILL", userId);
-        }
-
-        public static string Package(this List<string> raw)
-        {
-            return;
-            //return DataMap.Serialize(raw);
-        }
-    }
+    
 }
