9179719f2a2db89db03b93977052b4c2f28d9218
JuliaNaumets <julianaumets@github.com>
2015-05-22 21:44:12 +0300

    form register

--- Source/Client/clientExample/FormRegister.Designer.cs
+++ Source/Client/clientExample/FormRegister.Designer.cs
@@ -29,16 +29,135 @@
         private void InitializeComponent()
         {
             this.groupRegister = new System.Windows.Forms.GroupBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.textConfirmPass = new System.Windows.Forms.TextBox();
+            this.textNick = new System.Windows.Forms.TextBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkNickHelp = new System.Windows.Forms.LinkLabel();
+            this.labelRegister = new System.Windows.Forms.Label();
+            this.groupRegister.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupRegister
             // 
-            this.groupRegister.Location = new System.Drawing.Point(12, 30);
+            this.groupRegister.Controls.Add(this.labelRegister);
+            this.groupRegister.Controls.Add(this.linkNickHelp);
+            this.groupRegister.Controls.Add(this.label4);
+            this.groupRegister.Controls.Add(this.label3);
+            this.groupRegister.Controls.Add(this.label2);
+            this.groupRegister.Controls.Add(this.label1);
+            this.groupRegister.Controls.Add(this.buttonRegister);
+            this.groupRegister.Controls.Add(this.textNick);
+            this.groupRegister.Controls.Add(this.textConfirmPass);
+            this.groupRegister.Controls.Add(this.textPass);
+            this.groupRegister.Controls.Add(this.textEmail);
+            this.groupRegister.Location = new System.Drawing.Point(6, 6);
             this.groupRegister.Name = "groupRegister";
-            this.groupRegister.Size = new System.Drawing.Size(200, 100);
+            this.groupRegister.Size = new System.Drawing.Size(270, 161);
             this.groupRegister.TabIndex = 0;
             this.groupRegister.TabStop = false;
             this.groupRegister.Text = "Register";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(106, 26);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(156, 20);
+            this.textEmail.TabIndex = 1;
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(106, 52);
+            this.textPass.Name = "textPass";
+            this.textPass.Size = new System.Drawing.Size(156, 20);
+            this.textPass.TabIndex = 2;
+            this.textPass.UseSystemPasswordChar = true;
+            // 
+            // textConfirmPass
+            // 
+            this.textConfirmPass.Location = new System.Drawing.Point(106, 78);
+            this.textConfirmPass.Name = "textConfirmPass";
+            this.textConfirmPass.Size = new System.Drawing.Size(156, 20);
+            this.textConfirmPass.TabIndex = 3;
+            this.textConfirmPass.UseSystemPasswordChar = true;
+            // 
+            // textNick
+            // 
+            this.textNick.Location = new System.Drawing.Point(106, 104);
+            this.textNick.Name = "textNick";
+            this.textNick.Size = new System.Drawing.Size(156, 20);
+            this.textNick.TabIndex = 7;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(9, 130);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(253, 23);
+            this.buttonRegister.TabIndex = 0;
+            this.buttonRegister.Text = "Submit";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Confirm Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nickname:";
+            // 
+            // linkNickHelp
+            // 
+            this.linkNickHelp.AutoSize = true;
+            this.linkNickHelp.Location = new System.Drawing.Point(60, 107);
+            this.linkNickHelp.Name = "linkNickHelp";
+            this.linkNickHelp.Size = new System.Drawing.Size(13, 13);
+            this.linkNickHelp.TabIndex = 9;
+            this.linkNickHelp.TabStop = true;
+            this.linkNickHelp.Text = "?";
+            // 
+            // labelRegister
+            // 
+            this.labelRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelRegister.Location = new System.Drawing.Point(-4, 0);
+            this.labelRegister.Name = "labelRegister";
+            this.labelRegister.Size = new System.Drawing.Size(274, 161);
+            this.labelRegister.TabIndex = 10;
+            this.labelRegister.Text = "Registering...";
+            this.labelRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelRegister.Visible = false;
             // 
             // FormRegister
             // 
@@ -52,6 +171,8 @@
             this.Name = "FormRegister";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Register";
+            this.groupRegister.ResumeLayout(false);
+            this.groupRegister.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +180,16 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupRegister;
+        private System.Windows.Forms.LinkLabel linkNickHelp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.TextBox textNick;
+        private System.Windows.Forms.TextBox textConfirmPass;
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.TextBox textEmail;
+        public System.Windows.Forms.Label labelRegister;
     }
 }
--- Source/Client/clientExample/FormRegister.cs
+++ Source/Client/clientExample/FormRegister.cs
@@ -10,11 +10,71 @@ using System.Windows.Forms;
 
 namespace clientExample
 {
+    using System;
+    using System.Windows.Forms;
     public partial class FormRegister : Form
     {
         public FormRegister()
         {
             InitializeComponent();
+        }
+        public string Email { get { return textEmail.Text; } }
+        public string Password { get { return textPass.Text; } }
+        public string ConfirmPassword { get { return textConfirmPass.Text; } }
+
+        private void LinkNickHelpLinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Your nickname is the name that will appear to other users.");
+        }
+
+        private void ButtonRegisterClick(object sender, EventArgs e)
+        {
+            //textPass.Text = "123";
+            //textConfirmPass.Text = "1234";
+            if (string.IsNullOrEmpty(textEmail.Text))
+            {
+                MessageBox.Show("Please enter an email");
+                return;
+            }
+            if (string.IsNullOrEmpty(textNick.Text))
+            {
+                MessageBox.Show("Please enter a nickname");
+                return;
+            }
+            if (string.IsNullOrEmpty(textPass.Text))
+            {
+                MessageBox.Show("Please enter a password");
+                return;
+            }
+            if (textPass.Text != textConfirmPass.Text)
+            {
+                MessageBox.Show("Please ensure the password match");
+                textPass.Text = "";
+                textConfirmPass.Text = "";
+                return;
+            }
+            string buf = textPass.Text;
+            if (buf.Length < 5)
+            {
+                MessageBox.Show("Length your password must be more than 5 symbols");
+                return;
+            }
+            FormMain.Client.Connect(FormMain.IPADDRESS, FormMain.PORT);
+            var creq = Packets.CreateRequest(textEmail.Text, textPass.Text, textNick.Text);
+            FormMain.Client.WritePacket(creq);
+            labelRegister.Visible = true;
+        }
+
+        private void textPass_TextChanged(object sender, EventArgs e)
+        {
+            //textPass.PasswordChar = '*';
+            textPass.TextAlign = HorizontalAlignment.Center;
+        }
+
+        private void textConfirmPass_TextChanged(object sender, EventArgs e)
+        {
+            //textConfirmPass.PasswordChar = '*';
+            textConfirmPass.TextAlign = HorizontalAlignment.Center;
         }
     }
 }
